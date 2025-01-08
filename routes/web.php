385bbcf798bb7bc5eda328d9306f655a54dcf731
RidhoAji921 <ridhoaji921@gmail.com>
2025-01-08 17:35:12 +0700

    make controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', [AuthenticationController::class, 'login'])->name('login');
