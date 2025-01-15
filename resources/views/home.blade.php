691cb25eada56acc27ba2c38db9c5f7d923de7a8
RidhoAji921 <ridhoaji921@gmail.com>
2025-01-15 13:36:42 +0700

    tampilkan judul postingan terbaru

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,8 +40,11 @@
             <h2 class="text-xl font-semibold">Postingan Terbaru</h2>
             <div class="grid grid-cols-4 gap-1">
                 @forelse ($latestPosts as $post)
-                <a href="/posts/{{ $post->slug }}" class="aspect-square">
+                <a href="/posts/{{ $post->slug }}" class="relative aspect-square">
                     <img src="{{ "\\images\\".$post->thumbnail }}" alt="{{ $post->title }}" class="w-full h-full object-cover">
+                    <div class="absolute inset-0 flex items-end justify-center bg-black bg-opacity-20 opacity-0 hover:opacity-100 transition-opacity text-white text-xl font-bold pb-3">
+                        <p class="text-center">{{ $post->title }}</p>
+                    </div>
                 </a>
                 @empty
                     <p>Tidak ada postingan terbaru</p>
